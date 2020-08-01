7d2a93942d5b6172b17103f94eafcb3166496a9f
Asif Mohammad <asifratul45@gmail.com>
2020-08-01 10:42:50 +0600

    unit increment/decrement added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let unitPrice = [];
+const price = document.getElementsByClassName('price');
+for (let i = 0; i < price.length; i++) {
+    unitPrice[i] = parseFloat(price[i].innerText);    
+}
+
 // remove section
 var removeSection = document.getElementsByClassName('remove-item');
 for (let i = 0; i < removeSection.length; i++) {
@@ -7,5 +13,21 @@ for (let i = 0; i < removeSection.length; i++) {
     })
 }
 
-const iPhoneUnitPrice = parseFloat(document.getElementById('iPhone-price').innerText);
-const caseUnitPrice = parseFloat(document.getElementById('case-price').innerText);
+// FUNCTION to handle unit increment/decrement
+function changeUnit(increment, incrementValue) {
+    for (let i = 0; i < increment.length; i++) {
+        const incrementBtn = increment[i];
+        incrementBtn.addEventListener('click', function(event) {
+            const parentDiv = event.target.closest('div');
+            const childInput = parentDiv.querySelector('input');
+            const unit = parseInt(childInput.value) + incrementValue;
+            if(unit) childInput.value = unit;
+        })
+    }
+}
+
+// event handler for unit increment/decrement
+const plus = document.getElementsByClassName('plus');
+const minus = document.getElementsByClassName('minus');
+changeUnit(plus, 1);
+changeUnit(minus, -1);
